756117ca4ace055f804c067725ae4844f6663238
EdlynTo <to.edlyn@gmail.com>
2024-08-03 18:27:12 -0400

    webcam

--- api.py
+++ api.py
@@ -1,7 +1,9 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 import inference
 
 app = Flask(__name__)
+CORS(app)
 
 detector = inference.RestDetector('torchscript_model_0_66_49_wo_gl.pth')
 
@@ -9,6 +11,7 @@ detector = inference.RestDetector('torchscript_model_0_66_49_wo_gl.pth')
 def create_upload_file():
     file = request.files['file']
     emotion = detector.detect_emotion(file)
+    print(f"Detected emotion: {emotion}")
     return jsonify({"emotion": emotion})
 
 if __name__ == '__main__':
