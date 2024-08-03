95ffe67da7fc53e5c726a75ed4e153a40071c305
Jazzzny <jazzzny225@gmail.com>
2024-08-03 11:05:05 -0400

    push the stuff part 3

--- api.py
+++ api.py
@@ -1,12 +1,15 @@
-from fastapi import FastAPI, UploadFile
-
+from flask import Flask, request, jsonify
 import inference
 
-app = FastAPI()
+app = Flask(__name__)
 
 detector = inference.RestDetector('torchscript_model_0_66_49_wo_gl.pth')
 
-@app.post("/uploadfile/")
-async def create_upload_file(file: UploadFile):
-    emotion = detector.detect_emotion(file.file)
-    return {"emotion": emotion}
+@app.route('/uploadfile/', methods=['POST'])
+def create_upload_file():
+    file = request.files['file']
+    emotion = detector.detect_emotion(file)
+    return jsonify({"emotion": emotion})
+
+if __name__ == '__main__':
+    app.run(debug=True)
